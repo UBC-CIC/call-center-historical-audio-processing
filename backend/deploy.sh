92d922ae6e49246bb590dcecd9399dd3c1264d48
Neelim Novo <neelim98@gmail.com>
2021-07-14 15:28:23 -0700

    Update bash script parameter error

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 # ./deploy.sh --bucket-name <AWS_BUCKET_NAME> --aws-region <AWS_REGION> --aws-profile <AWS_PROFILE> --stack-name <STACK_NAME>
 
+bucketName=${2}
+awsRegion=${4}
+awsProfile=${6}
+stackName=${8}
+
 aws s3api create-bucket \
-  --bucket ${bucket-name} \
+  --bucket ${bucketName} \
   --create-bucket-configuration \
-  LocationConstraint=${aws-region:-us-west-2} \
-  --region ${aws-region:-us-west-2} \
-  --profile ${aws-profile:-default}
+  LocationConstraint=${awsRegion:-us-west-2} \
+  --region ${awsRegion:-us-west-2} \
+  --profile ${awsProfile:-default}
 
 sam build
 
-sam package --s3-bucket ${bucket-name} --output-template-file out.yaml --profile ${aws-profile:-default}
+sam package --s3-bucket ${bucketName} --output-template-file out.yaml --profile ${awsProfile:-default}
 
 sam deploy \
   --template-file out.yaml \
-  --stack-name ${stack-name} \
+  --stack-name ${stackName} \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-  --profile ${aws-profile:-default} --region ${aws-region:-us-west-2}
+  --profile ${awsProfile:-default} --region ${awsRegion:-us-west-2}
